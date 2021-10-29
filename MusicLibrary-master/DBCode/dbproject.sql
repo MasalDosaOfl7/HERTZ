79dea52cd7e382253edaafa3985835d9d0a0e3dc
MasalDosaOfl7 <19z254@psgtech.ac.in>
2021-10-29 20:27:58 +0530

    Database SQL Comments Removed
    Comments from the original code removed.

--- MusicLibrary-master/DBCode/dbproject.sql
+++ MusicLibrary-master/DBCode/dbproject.sql
@@ -1,6 +1,38 @@
 CREATE DATABASE  IF NOT EXISTS `dbproject` ;
 USE `dbproject`;
 
+-- Table structure for table `user`;
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `num_of_playlists` int(11) DEFAULT '0',
+  PRIMARY KEY (`userID`),
+  UNIQUE KEY `user_name_UNIQUE` (`user_name`)
+) AUTO_INCREMENT=1000;
+
+
+-- Table structure for table `song`;
+
+DROP TABLE IF EXISTS `song`;
+
+CREATE TABLE `song` (
+  `SongID` int(11) NOT NULL AUTO_INCREMENT,
+  `Title` varchar(255) DEFAULT NULL,
+  `Album` varchar(255) DEFAULT NULL,
+  `Artist` varchar(255) DEFAULT NULL,
+  `Composer` varchar(255) DEFAULT NULL,
+  `Genre` varchar(255) DEFAULT NULL,
+  `Filepath` varchar(1024) DEFAULT NULL,
+  PRIMARY KEY (`SongID`)
+) AUTO_INCREMENT = 1;
+
+
 -- Table structure for table `history`;
 
 DROP TABLE IF EXISTS `history`;
@@ -31,6 +63,20 @@ CREATE TABLE `likes` (
 ) ;
 
 
+-- Table structure for table `playlist_info`;
+
+DROP TABLE IF EXISTS `playlist_info`;
+
+CREATE TABLE `playlist_info` (
+  `playlist_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `playlist_name` varchar(255) DEFAULT NULL,
+  `userID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`playlist_ID`),
+  KEY `user_id_idx` (`userID`),
+  CONSTRAINT `fk_userID_playlist_info` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE
+) AUTO_INCREMENT = 1;
+
+
 -- Table structure for table `playlist`;
 
 DROP TABLE IF EXISTS `playlist`;
@@ -46,47 +92,7 @@ CREATE TABLE `playlist` (
 ) ;
 
 
--- Table structure for table `playlist_info`;
-
-DROP TABLE IF EXISTS `playlist_info`;
-
-CREATE TABLE `playlist_info` (
-  `playlist_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `playlist_name` varchar(255) DEFAULT NULL,
-  `userID` int(11) DEFAULT NULL,
-  PRIMARY KEY (`playlist_ID`),
-  KEY `user_id_idx` (`userID`),
-  CONSTRAINT `fk_userID_playlist_info` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ;
-
--- Table structure for table `song`;
-
-DROP TABLE IF EXISTS `song`;
-
-CREATE TABLE `song` (
-  `SongID` int(11) NOT NULL AUTO_INCREMENT,
-  `Title` varchar(255) DEFAULT NULL,
-  `Album` varchar(255) DEFAULT NULL,
-  `Artist` varchar(255) DEFAULT NULL,
-  `Composer` varchar(255) DEFAULT NULL,
-  `Genre` varchar(255) DEFAULT NULL,
-  `Filepath` varchar(1024) DEFAULT NULL,
-  PRIMARY KEY (`SongID`)
-) ;
 
 
--- Table structure for table `user`;
 
-DROP TABLE IF EXISTS `user`;
-
-CREATE TABLE `user` (
-  `userID` int(11) NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `first_name` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL,
-  `num_of_playlists` int(11) DEFAULT '0',
-  PRIMARY KEY (`userID`),
-  UNIQUE KEY `user_name_UNIQUE` (`user_name`)
-) ;
 
